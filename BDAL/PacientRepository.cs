6e3ed121dfb6828b37820fe9c1c6224a2b706278
Rosa Nunez <146019823+RosaNunezRivera@users.noreply.github.com>
2024-03-16 16:45:38 -0500

    Update PacientRepository.cs
    Update PacientRepository with CRUD operations

--- BDAL/PacientRepository.cs
+++ BDAL/PacientRepository.cs
@@ -1,5 +1,6 @@
-﻿using BEntities.Context;
+using BEntities.Context;
 using BEntities.Entities;
+using System.Net.Http.Headers;
 
 namespace BDAL
 {
@@ -7,10 +8,11 @@ namespace BDAL
     {
         PacientBookingContext pacientBookingContext = new PacientBookingContext();
 
-        public List<Pacient> GetPacientsRepo()
+        public List<Pacient> GetAllPacientsRepo()
         {
             return pacientBookingContext.Pacients.ToList();
         }
+
         public string AddPacient(Pacient pacientFormData)
         {
             if (pacientFormData != null)
@@ -20,6 +22,52 @@ namespace BDAL
                 return "success";
             }
             return "error";
+        }
+        public Pacient GetPacientByIdRepo(int id)
+        {
+            return pacientBookingContext.Pacients.FirstOrDefault(x => x.PacientID == id);
+        }
+
+        public string UpdatePacientRepo(Pacient pacientFormData)
+        {
+            Pacient pacToBeUpdated = pacientBookingContext.Pacients.FirstOrDefault(x => x.PacientID == pacientFormData.PacientID);
+
+            if (pacToBeUpdated != null)
+            {
+                pacToBeUpdated.PacientName = pacientFormData.PacientName;
+                pacToBeUpdated.Email = pacientFormData.Email;
+                pacToBeUpdated.PhoneNumber = pacientFormData.PhoneNumber;
+                pacToBeUpdated.DOB = pacientFormData.DOB;
+
+                //Mapper.Map(pac, pacToBeUpdated); Using automapper is only one line
+                pacientBookingContext.SaveChanges();
+                return "success";
+            }
+            return "error";
+        }
+
+        public string DeletePacientRepo(int pacId) 
+        {
+            var response = "";
+            try 
+            {
+                Pacient pacToBeDeleted = pacientBookingContext.Pacients.FirstOrDefault(x => x.PacientID == pacId);
+                if (pacToBeDeleted != null)
+                {
+                    pacientBookingContext.Pacients.Remove(pacToBeDeleted);
+                    pacientBookingContext.SaveChanges();
+                    response = "success";
+                }
+                else 
+                {
+                    response = "error: Pacient not found";
+                }
+            }
+            catch (Exception ex) 
+            {
+                response = $"error: {ex.Message}";
+            }
+            return response;
         }
     }
 }
